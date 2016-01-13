a33a18f915328c78b88440c7f211f654a030bb36
James O'Toole <jimmeyotoole@gmail.com>
2016-01-13 12:13:27 +1100

    changes

--- structstesting.go
+++ structstesting.go
@@ -14,19 +14,10 @@ func (r Rectangle) display() {
 }
 
 func main() {
-	r1 := Rectangle{4, 5, "rec1"}
+	var r1 = Rectangle{4, 5, "rec1"}
 	r1.display()
-	// fmt.Printf("Rectange 1", r1)
-	// fmt.Println()
 
 	r2 := Rectangle{length: 10, width: 20, name: "IM A BOX"}
-	// fmt.Printf("Rectange 2 = %+v", r2)
 	r2.display()
 
-	r3 := new(Rectangle)
-	r3.length = 2
-	r3.width = 4
-	r3.name = "hi there"
-	// fmt.Println(*r3)
-	r3.display()
 }
